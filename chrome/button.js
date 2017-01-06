c649ccd7dff6d2d57ba98d3b8f1248b8db68354e
Isaac Schemm <isaacschemm@gmail.com>
2017-01-06 13:22:33 -0600

    Add preferences (notification type; whether to reset when new window is opened)

--- chrome/button.js
+++ chrome/button.js
@@ -7,40 +7,69 @@ var observerObj = null;
 
 this.addEventListener("load", function () {
 	prefs = Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefService).getBranch("media.navigator.permission.");
+	var thisWindow = this;
 	var toolbarbutton = document.getElementById("webrtc-permissions-ui-toggle-1");
 
 	observerObj = {
 		observe: function (aSubject, aTopic, aData) {
-			console.log(arguments);
 			if ("nsPref:changed" == aTopic) {
 				var newValue = aSubject.getBoolPref(aData);
-				toolbarbutton.label = toolbarbutton.tooltipText = "WebRTC Auto " + (newValue ? "(On)" : "(Off)");
 
 				var message = "";
 				if (newValue) {
+					toolbarbutton.label = toolbarbutton.tooltipText = "WebRTC Auto (On)";
 					toolbarbutton.classList.add("setting-true");
-					message = "Automatic WebRTC connection has been turned on (permissions dialog disabled). This change will last until you open a new browser window.\nYou might need to refresh the current page.";
+					message = "Automatic WebRTC connection has been turned on (permissions dialog overridden).\nYou might need to refresh the current page.";
 				} else {
+					toolbarbutton.label = toolbarbutton.tooltipText = "WebRTC Auto (Off)";
 					toolbarbutton.classList.remove("setting-true");
 					message = "Automatic WebRTC connection has been turned off (permissions dialog re-enabled).";
 				}
 
-				try {
-					Components.classes['@mozilla.org/alerts-service;1']
-							.getService(Components.interfaces.nsIAlertsService)
-							.showAlertNotification(null, "WebRTC Permissions UI Toggle", message, false, '', null);
-				} catch(e) {
-					alert(message);
+				var type = Components.classes["@mozilla.org/preferences-service;1"]
+					.getService(Components.interfaces.nsIPrefService)
+					.getBranch("extensions.webrtc-permissions-ui-toggle.")
+					.getCharPref("notify-type");
+				switch (type) {
+					case "non-modal":
+						try {
+							Components.classes['@mozilla.org/alerts-service;1']
+									.getService(Components.interfaces.nsIAlertsService)
+									.showAlertNotification(null, "WebRTC Permissions UI Toggle", message, false, '', null);
+						} catch (e) {
+							alert(message);
+						}
+						break;
+					case "none":
+						break;
+					default:
+						try {
+							alert(message);
+						} catch (e) {
+							Components.classes['@mozilla.org/alerts-service;1']
+										.getService(Components.interfaces.nsIAlertsService)
+										.showAlertNotification(null, "WebRTC Permissions UI Toggle", message, false, '', null);
+						}
+						break;
 				}
 			}
 		}
 	};
-	console.log(prefs,observerObj)
+	
 	prefs.addObserver("", observerObj, false);
 
 	var value = prefs.getBoolPref("disabled");
 	if (value) {
-		prefs.setBoolPref("disabled", false);
+		toolbarbutton.label = toolbarbutton.tooltipText = "WebRTC Auto (On)";
+		toolbarbutton.classList.add("setting-true");
+		
+		var r = Components.classes["@mozilla.org/preferences-service;1"]
+					.getService(Components.interfaces.nsIPrefService)
+					.getBranch("extensions.webrtc-permissions-ui-toggle.")
+					.getBoolPref("reset-on-new-window");
+		if (r) {
+			prefs.setBoolPref("disabled", false);
+		}
 	} else {
 		toolbarbutton.label = toolbarbutton.tooltipText = "WebRTC Auto (Off)";
 	}
