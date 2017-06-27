7bd343e404953f15519dd1809ce51b9eafcbef54
Isaac Schemm <isaacschemm@gmail.com>
2017-06-27 09:15:37 -0500

    Allow insertion of script after page is loaded

--- extra/sm-webrtc-override-check.js
+++ extra/sm-webrtc-override-check.js
@@ -9,75 +9,83 @@
 */
 
 if (/SeaMonkey/.test(navigator.userAgent)) {
-    window.addEventListener("load", function () {
-        if (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia) return;
+	(function () {
+		var check = function () {
+			if (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia) return;
 
-        var webRTCSuccess = false;
+			var webRTCSuccess = false;
 
-        // Initial check
-        navigator.mediaDevices.getUserMedia({ video: false, audio: true }).then(function () {
-            webRTCSuccess = true;
-        });
+			// Initial check
+			navigator.mediaDevices.getUserMedia({ video: false, audio: true }).then(function () {
+				webRTCSuccess = true;
+			});
 
-        // Wait 1 second
-        setTimeout(function () {
-            if (webRTCSuccess) return;
+			// Wait 1 second
+			setTimeout(function () {
+				if (webRTCSuccess) return;
 
-            // WebRTC request was not accepted; show message
-            var cover = document.createElement("div");
-            cover.id = "sm-webrtc-override-check";
-            var cover_s = cover.style;
-            cover_s.position = "absolute";
-            cover_s.width = cover_s.height = "100%";
-            cover_s.left = cover_s.top = "0";
-            cover_s.backgroundColor = "rgba(0, 0, 0, 0.5)";
-            cover_s.display = "flex";
-            cover_s.alignItems = cover_s.justifyContent = "center";
-            document.body.appendChild(cover);
+				// WebRTC request was not accepted; show message
+				var cover = document.createElement("div");
+				cover.id = "sm-webrtc-override-check";
+				var cover_s = cover.style;
+				cover_s.position = "absolute";
+				cover_s.width = cover_s.height = "100%";
+				cover_s.left = cover_s.top = "0";
+				cover_s.backgroundColor = "rgba(0, 0, 0, 0.5)";
+				cover_s.display = "flex";
+				cover_s.alignItems = cover_s.justifyContent = "center";
+				document.body.appendChild(cover);
 
-            var box = document.createElement("div");
-            var box_s = box.style;
-            box_s.maxWidth = "500px";
-            box_s.padding = "25px";
-            box_s.backgroundColor = "white";
-            box_s.color = "black";
-            box_s.borderRadius = "5px";
-            cover.appendChild(box);
+				var box = document.createElement("div");
+				var box_s = box.style;
+				box_s.maxWidth = "500px";
+				box_s.padding = "25px";
+				box_s.backgroundColor = "white";
+				box_s.color = "black";
+				box_s.borderRadius = "5px";
+				cover.appendChild(box);
 
-            box.innerHTML = "To use WebRTC in SeaMonkey, install <a href='https://github.com/IsaacSchemm/webrtc-permissions-ui-toggle#readme'>WebRTC Permissions UI Toggle</a>, toggle it on, and reload this page; or try another browser, such as Firefox or Chrome.";
+				box.innerHTML = "To use WebRTC in SeaMonkey, install <a href='https://github.com/IsaacSchemm/webrtc-permissions-ui-toggle#readme'>WebRTC Permissions UI Toggle</a>, toggle it on, and reload this page; or try another browser, such as Firefox or Chrome.";
 
-            var ignore_p = document.createElement("p");
-            ignore_p.align = "center";
-            ignore_p.style.marginBottom = "0";
-            box.appendChild(ignore_p);
+				var ignore_p = document.createElement("p");
+				ignore_p.align = "center";
+				ignore_p.style.marginBottom = "0";
+				box.appendChild(ignore_p);
 
-            var ignore = document.createElement("button");
-            ignore.innerHTML = "Ignore";
-            ignore.style.minWidth = "100px";
-            ignore_p.appendChild(ignore);
+				var ignore = document.createElement("button");
+				ignore.innerHTML = "Ignore";
+				ignore.style.minWidth = "100px";
+				ignore_p.appendChild(ignore);
 
-            function close(mediaStream) {
-                webRTCSuccess = true;
-				if (mediaStream) {
-					// We don't need this stream, so close it.
-					mediaStream.getAudioTracks().forEach(function (t) {
-						t.stop();
-					});
-                }
-                document.body.removeChild(cover);
-            }
+				function close(mediaStream) {
+					webRTCSuccess = true;
+					if (mediaStream) {
+						// We don't need this stream, so close it.
+						mediaStream.getAudioTracks().forEach(function (t) {
+							t.stop();
+						});
+					}
+					document.body.removeChild(cover);
+				}
 
-            ignore.addEventListener("click", function () {
-                close();
-            });
+				ignore.addEventListener("click", function () {
+					close();
+				});
 
-            // Retry WebRTC every time the mouse cursor leaves and re-enters the window
-            document.body.addEventListener("mouseenter", function () {
-                if (webRTCSuccess) return;
-                setTimeout(function () {
-                    navigator.mediaDevices.getUserMedia({ video: false, audio: true }).then(close);
-                }, 0);
-            });
-        }, 1000);
-    });
+				// Retry WebRTC every time the mouse cursor leaves and re-enters the window
+				document.body.addEventListener("mouseenter", function () {
+					if (webRTCSuccess) return;
+					setTimeout(function () {
+						navigator.mediaDevices.getUserMedia({ video: false, audio: true }).then(close);
+					}, 0);
+				});
+			}, 1000);
+		};
+		var interval = setInterval(function () {
+			if (/interactive|complete/.test(document.readyState)) {
+				clearInterval(interval);
+				check();
+			}
+		}, 100);
+	})();
 }
